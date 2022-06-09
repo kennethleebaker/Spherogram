4d2eb605d69c95ee9567e7abaa01476629c05071
patriciacahn <93236582+patriciacahn@users.noreply.github.com>
2022-06-09 12:07:51 -0500

    added dbc_tangle to dbc.py (#13)
    * added dbc_tangle to dbc.py
    
    * added option to specify index of unknot component in dbc.py

--- spherogram_src/links/dbc.py
+++ spherogram_src/links/dbc.py
@@ -1,20 +1,34 @@
-def double_branched_cover(M):
+def double_branched_cover(M,n=None):
     """
-    Input: Annular link complement corresponding to a (2,2) tangle T
-    Output: Double branched cover of T
+    Input: Complement M of a link L where L has one unknot component U 
+        such that the geometric intersection number of L-U with U is +/- 2.
+        n is the index of the unknont component; last component by default.
+    
+    Given a (2,2) tangle T, the annular closure of T, together with its axis U, is such a link L. 
+    double_branched_cover assumes the axis U is the last component of L.
+    
+    Output: Double branched cover of the exterior of T.
     """
     
-    #later change input from M, link exterior (annular closure of T), to T itself and call annular closure function
+    
     num_components=M.num_cusps()
     
-    #assume last component is one we don't fill
-    fill_list=[(2,0)]*(num_components-1)+[(0,0)]
+    if num_components  < 2:
+        raise ValueError("Number of cusps must be at least 2.")
+        
+    if n==None:
+        n=num_components-1
+    
+    #assume last cusp is one we don't fill
+    fill_list=[(2,0)]*(num_components)
+    fill_list[n]=(0,0)
+    
     
     
     M.dehn_fill(fill_list)
     covers_list=M.covers(2)
     
-    #Look for cover with 2 unfilled components and all others with filling (1,0)
+    #Look for cover with 2 unfilled cusps and all others with filling (1,0)
     good_covers_list=[]
     
     for C in covers_list:
@@ -60,3 +74,25 @@ def double_branched_cover(M):
         good_cover_f=good_cover.filled_triangulation()
         
         return(good_cover_f)
+
+def dbc_tangle(T):
+    """
+    Input T is a spherogram (2,2)-tangle.
+    Output is the double branched cover of the complement of T.
+    
+    >>>import snappy
+    >>>TestTangle = BraidTangle([2, 2, 1, 1, 2, 2, 1, 1, 2]) + IdentityBraid(1)
+    >>>TestTangle.annular_closure(-1).exterior().identify()
+    [v3383(0,0)(0,0)(0,0), 8^3_1(0,0)(0,0)(0,0), L8a18(0,0)(0,0)(0,0)]
+    
+    >>>M=dbc_tangle(TestTangle)
+    >>>M.identify()
+    [m004(0,0), 4_1(0,0), K2_1(0,0), K4a1(0,0), otet02_00001(0,0)]
+    """
+    #check that T is (2,2)
+    if T.boundary != (2,2):
+        raise Exception("Not a (2,2)-tangle.")
+    
+    
+    M=T.annular_closure(-1).exterior()
+    return double_branched_cover(M)
